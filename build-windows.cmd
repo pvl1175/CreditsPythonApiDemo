83d12c7c5fe278948954306ff26cf141581c9387
vitaly popov <vitaly.popov.75@gmail.com>
2019-04-22 20:36:10 +0300

    update

---
+++ build-windows.cmd
@@ -0,0 +1,8 @@
+git clone https://github.com/CREDITSCOM/CS-API
+thrift -gen py -out . .\cs-api\general.thrift
+thrift -gen py -out . .\cs-api\api.thrift
+python -m venv env
+call env\Scripts\activate.bat
+pip install thrift
+pip install base58
+exit
